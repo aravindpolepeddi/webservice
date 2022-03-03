a1cf50f386fb61b948340ad697a073252fe3e233
Aravind Polepeddi <polepeddi.a@northeastern.edu>
2022-03-03 10:09:35 -0500

    need to run

---
+++ shell/node.sh
@@ -0,0 +1,21 @@
+sleep 30
+
+
+
+sudo yum update -y
+
+
+
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install -y nodejs
+
+
+
+sudo yum install unzip
+unzip /home/ec2-user/webservice1.zip
+cd webservice/
+npm i
+
+
+
---
+++ shell/postgresql.sh
@@ -0,0 +1,61 @@
+sleep 30
+
+
+
+sudo yum update -y
+sudo amazon-linux-extras install postgresql9.6
+
+
+
+sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
+[pgdg13]
+name=PostgreSQL 13 for RHEL/CentOS 7 - x86_64
+baseurl=http://download.postgresql.org/pub/repos/yum/13/redhat/rhel-7-x86_64
+enabled=1
+gpgcheck=0
+EOF
+
+
+
+
+sudo yum install postgresql13 postgresql13-server -y
+sudo /usr/pgsql-13/bin/postgresql-13-setup initdb
+
+
+
+sudo systemctl stop postgresql-13.service
+sudo systemctl start postgresql-13.service
+sudo systemctl enable postgresql-13.service
+sudo systemctl status postgresql-13.service
+
+
+
+sudo -u psql <<EOF
+\x
+ALTER ROLE postgres WITH PASSWORD '@uest123';
+CREATE DATABASE "postgres";
+\q
+EOF
+
+sudo -u psql <<EOF
+\x
+CREATE TABLE IF NOT EXISTS users
+(
+id uuid NOT NULL DEFAULT uuid_generate_v4(),
+first_name VARCHAR(255),
+last_name VARCHAR(255),
+password VARCHAR(255),
+username VARCHAR(255),
+account_created timestamp,
+account_updated timestamp,
+CONSTRAINT users_pkey PRIMARY KEY (id),
+CONSTRAINT users_username_key UNIQUE (username)
+);
+\q
+EOF
+
+
+
+sudo systemctl stop postgresql-13.service
+sudo systemctl start postgresql-13.service
+sudo systemctl status postgresql-13.service
