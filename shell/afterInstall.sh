c1ef6ddda0c54c5db71e25858002d46e29173353
Aravind Polepeddi <polepeddi.a@northeastern.edu>
2022-04-06 16:18:08 -0400

    pckr test

--- shell/afterInstall.sh
+++ shell/afterInstall.sh
@@ -2,3 +2,15 @@ cd /home/ec2-user
 npm i
  
 sleep 10
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+
+
