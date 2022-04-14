2d634bac7c5d8c0390a6e08f7e2c77f271af1538
Aravind Polepeddi <polepeddi.a@northeastern.edu>
2022-04-14 13:46:40 -0400

    verified changes

--- database.js
+++ database.js
@@ -1,21 +1,21 @@
 const Pool = require('pg').Pool;
-
+/*
 const pool = new Pool({
     host: process.env.DB_CONNECTION,
     user: process.env.DB_USERNAME,
     port: process.env.PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-});
+});*/
 
 
-/*
+
 const pool = new Pool({
 host: "localhost",
 user: "postgres",
 port: "5432",
 password: "@uest123!",
 database: "postgres"
-})*/
+})
 
 module.exports = pool;
